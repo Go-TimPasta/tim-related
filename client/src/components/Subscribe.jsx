1c54e6a4eff1d0b59900cb427a810bb1f162c274
Justin Murakami <jmurakami84@gmail.com>
2020-10-05 13:41:31 -0700

    modified logic for using view state to render the form or one of the response messages (already subscribed or new subscriber). added some styling to the overall component and the form.

--- client/src/components/Subscribe.jsx
+++ client/src/components/Subscribe.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const StyledSubscribeContainer = styled.div`
+  background-color: #D7E6F5;
+  text-align: center;
+  font-family: Arial;
+  font-size: 14px;
+  padding-bottom: 36px
+`;
+
+const StyledSubscribeInput = styled.input`
+  border: none;
+  outline: none;
+`;
+
+const StyledSubscribeButton = styled.button`
+  border: none;
+  outline: none;
+  background-color: white;
+  transition: all 200ms ease-out;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(239, 239, 239);
+  }
+`;
+
+const StyledInvitationMessage = styled.h3`
+  padding-top: 36px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
 
 export default class Subscribe extends React.Component {
   constructor(props) {
@@ -62,44 +94,41 @@ export default class Subscribe extends React.Component {
   }
 
   render() {
-    if (this.state.view === 'form' || this.state.view === 'invalid') {
-      return (
-        <div>
-          <h3>Yes! Send me exclusive offers, unique gift ideas, and personalized tips for shopping and selling on Getsy.</h3>
-          <form>
-            <input type="text" placeholder="Enter your email" onChange={(e) => { this.handleChange(e); }} />
-            <button type="submit" onClick={(e) => { this.handleClick(e); }}>Subscribe</button>
-          </form>
-          <p>Please enter a valid email address.</p>
-        </div>
-      );
-    }
-    if (this.state.view === 'invalid') {
-      return (
-        <div>
-          <h3>Yes! Send me exclusive offers, unique gift ideas, and personalized tips for shopping and selling on Getsy.</h3>
-          <form>
-            <input type="text" placeholder={this.state.email} onChange={(e) => { this.handleChange(e); }} />
-            <button type="submit">Subscribe</button>
-          </form>
-        </div>
-      );
-    }
-    if (this.state.view === 'yes') {
-      return (
-        <div>
-          <h3>Yes! Send me exclusive offers, unique gift ideas, and personalized tips for shopping and selling on Getsy.</h3>
-          <h3>Looks like you have an account!  Please Log in to subscribe.</h3>
-        </div>
-      );
-    }
-    if (this.state.view === 'no') {
-      return (
-        <div>
-          <h3>Yes! Send me exclusive offers, unique gift ideas, and personalized tips for shopping and selling on Getsy.</h3>
-          <h3>Great! We've sent you an email to confirm your subscription.</h3>
-        </div>
-      );
-    }
+    const content = (view) => {
+      if (view === 'form' || view === 'invalid') {
+        return (
+          <div>
+            <form>
+              <StyledSubscribeInput type="text" placeholder="Enter your email" onChange={(e) => { this.handleChange(e); }} />
+              <StyledSubscribeButton type="submit" onClick={(e) => { this.handleClick(e); }}>Subscribe</StyledSubscribeButton>
+            </form>
+            <p>Please enter a valid email address.</p>
+          </div>
+        );
+      }
+      if (view === 'yes') {
+        return (
+          <div>
+            <h3>Looks like you have an account!  Please Log in to subscribe.</h3>
+          </div>
+        );
+      }
+      if (view === 'no') {
+        return (
+          <div>
+            <h3>Great! We've sent you an email to confirm your subscription.</h3>
+          </div>
+        );
+      }
+    };
+
+    const subscribeContent = content(this.state.view);
+
+    return (
+      <StyledSubscribeContainer>
+        <StyledInvitationMessage>Yes! Send me exclusive offers, unique gift ideas, and personalized tips for shopping and selling on Getsy.</StyledInvitationMessage>
+        {subscribeContent}
+      </StyledSubscribeContainer>
+    );
   }
 }
