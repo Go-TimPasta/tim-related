915d3644d1e31056d0ace75016298a4ba124a2cf
timkang93 <jeongmo.kang@gmail.com>
2020-10-28 15:28:15 -0700

    edit before EC2 deployment

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 export let options = {
   stages: [
-    { duration: '10s', target: 450 },
-    { duration: '10m', target: 450 },
+    { duration: '10s', target: 700 },
+    { duration: '20m', target: 700 },
   ],
 };
 
