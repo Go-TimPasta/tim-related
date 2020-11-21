427022796383d9112a1b5ca599b9ac29e4e45b90
timkang93 <jeongmo.kang@gmail.com>
2020-11-20 17:52:17 -0800

    [style] remove comments

--- server/index.js
+++ server/index.js
@@ -1,40 +1,16 @@
-const newrelic = require('newrelic');
-// const redis = require('redis');
 const express = require('express');
-const compression = require('compression');
 const morgan = require('morgan');
-const cors = require('cors');
 const bodyparser = require('body-parser');
 const path = require('path');
-const router = require('./router');
 
-// const client = redis.createClient();
 const server = express();
 const port = 8005;
+const router = require('./router');
 
 server.use(morgan('dev'));
-server.use(cors());
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({ extended: true }));
-server.use(compression());
-
-// const redisMiddleware = (req, res, next) => {
-//   const key = '__express__' + req.originalUrl || req.url;
-//   client.get(key, (err, reply) => {
-//     if (reply) {
-//       res.send(reply);
-//     } else {
-//       res.sendResponse = res.send;
-//       res.send = (body) => {
-//         client.set(key, JSON.stringify(body));
-//         res.sendResponse(body);
-//       };
-//       next();
-//     }
-//   });
-// };
-
 server.use(express.static(path.join(__dirname, '../client/dist')));
-console.log('here');
 server.use('/related', router);
+
 server.listen(port, () => console.log(`listening on ${port}`));
